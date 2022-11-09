848acc92a3bdf2b05d130199c87b6dcc3025e6b2
KacperMazurWSiZ <w66057@student.wsiz.edu.pl>
2022-11-09 20:15:03 +0100

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] randomNumbersArray = new int[10];
+        getRandomNumber(randomNumbersArray);
+        displaySumAndAvgFromRandomNumbersArray(randomNumbersArray);
+
+
+
+
+    }
+
+    public static int[] getRandomNumber(int randomNumbersArray[]) {
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            randomNumbersArray[i] = random.nextInt(100) + 1;
+        }
+        return randomNumbersArray;
+    }
+
+    public static void displaySumAndAvgFromRandomNumbersArray(int randomNumbersArray[]) {
+        double sum = 0;
+        double avg = 0;
+
+        for (int i = 0; i < randomNumbersArray.length; i++) {
+            sum += randomNumbersArray[i];
+        }
+
+        avg = sum / randomNumbersArray.length;
+
+        System.out.println("Suma wynosi: " + sum);
+        System.out.println("Srednia wynosi: " + avg);
     }
 }
