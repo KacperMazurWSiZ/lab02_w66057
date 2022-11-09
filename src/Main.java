ca02c4f698e80b4f06d32a5bb8ecb1363a48ad1b
KacperMazurWSiZ <w66057@student.wsiz.edu.pl>
2022-11-09 20:21:55 +0100

    Task 2 and 3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        int[] randomNumbersArray = new int[10];
-        getRandomNumber(randomNumbersArray);
-        displaySumAndAvgFromRandomNumbersArray(randomNumbersArray);
+        // ZADANIE 1
+//        int[] randomNumbersArray = new int[10];
+//        getRandomNumber(randomNumbersArray);
+//        displaySumAndAvgFromRandomNumbersArray(randomNumbersArray);
+
+        // ZADANIE 2
+
+//        displayValueFromArrays();
+
+        // ZADANIE 3
+//        changeToUppercase();
+
+        //ZADANIE 4
+        //ZADANIE 5
+        //ZADANIE 6
+        //ZADANIE 7
 
 
 
@@ -30,5 +43,35 @@ public class Main {
 
         System.out.println("Suma wynosi: " + sum);
         System.out.println("Srednia wynosi: " + avg);
+    }
+
+    public static void displayValueFromArrays(){
+        int [] firstArray = {1,2,3,4,5,6,7,8,9};
+        int [] secondaryArray = {1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println("Nieparzysta tablica:");
+
+        for(int i = 0; i < firstArray.length; i++){
+            if(firstArray[i]%2==0){
+                System.out.println(firstArray[i]);
+            }
+        }
+
+        System.out.println("Parzysta tablica:");
+
+        for(int i = 0; i < secondaryArray.length; i++){
+            if(secondaryArray[i]%2==0){
+                System.out.println(secondaryArray[i]);
+            }
+        }
+    }
+
+    public static void changeToUppercase(){
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        for (String c: cars ) {
+            System.out.println(c.toUpperCase());
+        }
+
     }
 }
